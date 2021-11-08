8aad6b4261c61e25153f6205e54c1acf1b59f385
Daniel E. Parra M <depm1123@gmail.com>
2021-11-08 18:36:49 -0500

    pre & post gen_project files modified

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,1 +1,14 @@
-print(f"New project has been generated")
+import subprocess
+
+from hooks.pre_gen_project import RESET_ALL
+
+MESSAGE_COLOR = "\x1b[33m"
+RESET_ALL = "\x1b[0m"
+
+print("Initializing a Git repository {RESET_ALL}")
+
+subprocess.call(["git", "init"])
+subprocess.call(["git", "add", "."])
+subprocess.call(["git", "commit", "-m", "Initial commit"])
+
+print(f"{MESSAGE_COLOR} ¡Project generated succesfully!")
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,4 +1,11 @@
 
-MESSAGE_COLOR = "\x1b[34m"
+import os
+import sys
+
+project_slug = "{{ cookiecutter.project_slug }}"
+
+MESSAGE_COLOR = "\x1b[32m"
+RESET_ALL = "\x1b[0m"
 
 print(f"{MESSAGE_COLOR}Initializing a new project")
+print(f"Creating project at {os.getcwd()}{RESET_ALL}")
